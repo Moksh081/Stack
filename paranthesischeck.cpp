3fd9f58c77fd6fc0b5178f4ee38511ff5e751aa7
Moksh Sharma <109467184+Moksh081@users.noreply.github.com>
2023-10-08 11:58:13 +0530

    Add files via upload

---
+++ paranthesischeck.cpp
@@ -0,0 +1,91 @@
+//parenthesis matching using array
+/*
+
+ #include<iostream>
+ #include<cstring>
+using namespace std;
+ 
+class Stack{
+private:
+    int size;
+    int top;
+    char* S;
+public:
+    Stack(int size);
+    ~Stack();
+    void push(char x);
+    int pop();
+    int isFull();
+    int isEmpty();
+};
+ 
+Stack::Stack(int size) {
+    this->size = size;
+    top = -1;
+    S = new char[size];
+}
+ 
+Stack::~Stack() {
+    delete[] S;
+}
+
+void Stack :: push(char x){
+	if(isFull()){
+		cout<<"Overflow"<<endl;
+	}else{
+		top++;
+		S[top] = x;
+	}
+}
+
+int Stack :: pop(){
+	int x;
+	if(isEmpty()){
+		cout<<"Underflow"<<endl;
+	}else{
+		x = S[top];
+		top--;
+	}
+	return x;
+}
+
+
+	int Stack :: isFull(){
+    return (top == size - 1);
+}
+
+
+int Stack :: isEmpty(){
+	if(top == -1){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+ bool isBalanced(char* exp){
+    Stack stk(strlen(exp));
+    for (int i=0; i<strlen(exp); i++){
+        if (exp[i] == '(' || exp[i]=='{' || exp[i]=='['){
+            stk.push(exp[i]);
+        } else if (exp[i] == ')'|| exp[i]=='}' || exp[i]==']'){
+            if (stk.isEmpty()){
+                return false;
+            } else {
+                stk.pop();
+            }
+        }
+    }
+    return stk.isEmpty();
+}
+
+ 
+int main() {
+ 
+    char E[] = "({[})";
+    cout << isBalanced(E) << endl;
+ 
+    return 0;
+}
+ 
+*/
